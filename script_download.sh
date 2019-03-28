a1136ae20fcf0649431ab970e95421d43af66626
T. Cameron Waller <tcameronwaller@gmail.com>
2019-03-28 11:44:39 -0700

    still playing with how to make remote

---
+++ script_download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+# Echo each command to console.
+set -x
+
+wget https://github.com/tcameronwaller/bimodality/archive/master.zip
+unzip master.zip
+cd bimodality-master/
+
