57092ec16daa114aa13cd63944d57c2056f8d311
T. Cameron Waller <tcameronwaller@gmail.com>
2019-04-02 13:41:36 -0700

    customized execution script for server directory structure

---
+++ execute_program_server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+path_project="/cellar/users/tcwaller/"
+subpath_repository="temporary/bimodality_master/bimodality/"
+path_repository="$path_project$subpath_repository/"
+subpath_program="temporary/bimodality_master/bimodality/"
+path_program="$path_project$subpath_program"
+subpath_dock="Data/dock/"
+path_dock="$path_project$subpath_dock"
+
+# Suppress echo each command to console.
+set +x
+
+echo "--------------------------------------------------"
+echo "----------"
+echo "Here are your working directories."
+echo "project: $path_project"
+echo "repository: $path_repository"
+echo "dock: $path_dock"
+echo "----------"
+echo "--------------------------------------------------"
+
+# Echo each command to console.
+set -x
+
+cd $path_project
+mkdir $path_dock
+cd $path_program
+
+echo "Now set to call routine and procedures."
+
+python3 interface.py main -d $path_dock -a
+python3 interface.py main -d $path_dock -b
+python3 interface.py main -d $path_dock -s
+#python3 interface.py main -d $path_dock -o
+#python3 interface.py main -d $path_dock -n
+#python3 interface.py main -d $path_dock -m
+#python3 interface.py main -d $path_dock -t
