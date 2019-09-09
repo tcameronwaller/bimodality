8527a302236b714962d051803cd8fd89875bd84d
T. Cameron Waller <tcameronwaller@gmail.com>
2019-09-09 16:02:41 -0700

    corrected file path for output

--- execution_nrnb.sh
+++ execution_nrnb.sh
@@ -6,8 +6,10 @@
 export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
 path_user_cellar="/cellar/users/tcwaller"
 path_user_nrnb="/nrnb/users/tcwaller"
+subpath_dock="gtex_genotype/dock"
 subpath_persons="Data/dock/selection/persons.txt"
 subpath_genotype="gtex_genotype/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.vcf.gz"
+path_dock="$path_user_nrnb/$subpath_dock"
 path_persons="$path_user_cellar/$subpath_persons"
 path_genotype="$path_user_nrnb/$subpath_genotype"
 
@@ -23,4 +25,6 @@ echo "--------------------------------------------------"
 # Echo each command to console.
 set -x
 
-plink2 --vcf $path_genotype --keep $path_persons --maf 0.01 --make-pgen --out fileset
+mkdir $path_dock
+
+plink2 --vcf $path_genotype --keep $path_persons --maf 0.01 --make-pgen --out $path_dock/fileset
