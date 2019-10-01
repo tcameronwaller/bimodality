50b046bb709d57b1f54aca7f7c91781135f0c4ea
T. Cameron Waller <tcameronwaller@gmail.com>
2019-09-30 20:06:34 -0700

    work on initial script for heritability

--- execution_heritability_initial.sh
+++ execution_heritability_initial.sh
@@ -4,20 +4,22 @@
 
 # Organize paths.
 export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
-path_user_cellar="/cellar/users/tcwaller"
-path_user_nrnb="/nrnb/users/tcwaller"
-
 path_gcta="$path_user_cellar/gcta_1.92.3beta3/gcta64"
 path_plink="$path_user_cellar/plink2"
-path_dock="$path_user_nrnb/dock"
 
-path_persons="$path_user_cellar/Data/heritability/families_persons.tsv"
-path_genotype_vcf="$path_user_nrnb/gtex_genotype/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.vcf.gz"
-path_genotype_ped="$path_dock/gtex-8_genotype"
-path_distribution="$path_user_cellar/Data/heritability/families_persons_signals.tsv"
-path_relation="$path_dock/relation"
-path_result="$path_dock/result"
+path_user_cellar="/cellar/users/tcwaller"
+path_user_nrnb="/nrnb/users/tcwaller"
+path_genotype="$path_user_nrnb/gtex_genotype"
+path_dock="$path_user_cellar/Data/dock"
+path_selection="$path_dock/selection"
+path_persons="$path_selection/families.tsv"
+path_heritability="$path_dock/heritability"
+path_relation="$path_heritability/relation"
+path_genotype_vcf="$path_genotype/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.vcf.gz"
+path_genotype_ped="$path_genotype/gtex-8_genotype"
 
+rm -r $path_relation
+mkdir $path_relation
 
 # Suppress echo each command to console.
 set +x
@@ -31,16 +33,7 @@ echo "--------------------------------------------------"
 # Echo each command to console.
 set -x
 
-#rm -r $path_genotype_ped
-#mkdir $path_genotype_ped
-
-rm -r $path_relation
-mkdir $path_relation
-
 ##########
-# Only convert data format.
-#$path_bin/plink2 --vcf $path_genotype --out $path_dock/gtex-8_genotype
-
 # Convert data format.
 # Possible to use PLINK to filter by person and minimal allelic frequency.
 #$path_plink --vcf $path_genotype --keep $path_persons --maf 0.01 --make-pgen --out $path_dock/gtex-8_genotype
@@ -48,18 +41,7 @@ mkdir $path_relation
 # GCTA requires PLINK 1 format files, .bed, .bim, and .fam.
 #$path_plink --vcf $path_genotype_vcf --no-fid --make-bed --out $path_genotype_ped
 # I think that the "no-fid" flag does not change anything when importing a VCF.
-#$path_plink --vcf $path_genotype_vcf --make-bed --out $path_genotype_ped/genotype --threads 20
-
-# Calculate genetic relationship matrix (GRM).
-# For cis heritability, GRM must be specific to each gene's chromosome.
-# Filter by persons and minimal allelic frequence (MAF).
-# GCTA's format requirement for list of persons is text with tab delimiters.
-#$path_gcta --bfile $path_dock/gtex-8_genotype --autosome --maf 0.01 --make-grm --out $path_dock/gtex-8_grm_autosomes --threads 10
-#$path_gcta --bfile $path_genotype_ped --keep $path_persons --chr 6 --maf 0.01 --make-grm --out $path_relation/chromosome_6 --threads 10
-
-
-# TODO: create cis and trans GRMs for each chromosome
-# chromosome 6 --> 6_cis (chrom 6 only), 6_trans (all autosomes other than 6)
+$path_plink --vcf $path_genotype_vcf --make-bed --out $path_genotype_ped --threads 10
 
 ##########
 # Generate GRM for all autosomal chromosomes.
@@ -68,18 +50,21 @@ mkdir $path_relation
 ##########
 # Generate GRMs for individual chromosomes.
 # These GRMs are directly useful as cis GRMs.
-path_cis="$path_heritability/cis"
+path_cis="$path_relation/cis"
 rm -r $path_cis
 mkdir $path_cis
 # Iterate across autosomal chromosomes.
 for cis in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
-    $path_gcta --bfile $path_genotype_ped/genotype --keep $path_persons --chr $cis --maf 0.01 --make-grm --out $path_cis/$cis --threads 20
+    # For cis heritability, GRM must be specific to each gene's chromosome.
+    # Filter by persons and minimal allelic frequence (MAF).
+    # GCTA's format requirement for list of persons is text with tab delimiters.
+    $path_gcta --bfile $path_genotype_ped --keep $path_persons --chr $cis --maf 0.01 --make-grm --out $path_cis/$cis --threads 10
 done
 
 ##########
 # Define combinations of chromosomes for unification of trans GRMs.
-path_combinations="$path_heritability/combinations"
+path_combinations="$path_relation/combinations"
 rm -r $path_combinations
 mkdir $path_combinations
 # Iterate across cis autosomal chromosomes.
@@ -107,7 +92,7 @@ done
 # Unify GRMs for combinations of trans chromosomes
 # multi_grm.txt file should include complete paths to all individual grms to unify...
 
-path_trans="$path_heritability/trans"
+path_trans="$path_relation/trans"
 rm -r $path_trans
 mkdir $path_trans
 # Iterate across cis autosomal chromosomes.
