c0d67de74cfc52e36bda5d0b6f2fb6e581f8a5c4
T. Cameron Waller <tcameronwaller@gmail.com>
2019-10-01 11:21:24 -0700

    increased threads

--- execution_heritability_genes.sh
+++ execution_heritability_genes.sh
@@ -4,21 +4,24 @@
 #SBATCH --output=/cellar/users/tcwaller/Data/dock/heritability_out.txt
 #SBATCH --error=/cellar/users/tcwaller/Data/dock/heritability_error.txt
 #SBATCH --mem=5G
-#SBATCH --array=0-15445%50
+#SBATCH --array=0-15445%10
 #SBATCH --time=2-00:00:00 # days-hours:minutes:seconds
 
 # Organize paths.
 export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
-path_project="/cellar/users/tcwaller"
-subpath_repository="repository/bimodality-master/bimodality"
-path_repository="$path_project/$subpath_repository"
-subpath_program="repository/bimodality-master/bimodality"
-path_program="$path_project/$subpath_program"
-subpath_dock="Data/dock"
-path_dock="$path_project/$subpath_dock"
+path_user_cellar="/cellar/users/tcwaller"
+path_python="$path_user_cellar/anaconda3/bin/python"
+path_gcta="$path_user_cellar/gcta_1.92.3beta3/gcta64"
+path_plink="$path_user_cellar/plink2"
+path_repository="$path_user_cellar/repository"
+path_program="$path_repository/bimodality-master/bimodality"
+path_dock="$path_user_cellar/Data/dock"
+path_split="$path_dock/split"
+path_heritability="$path_dock/heritability"
+path_relation="$path_heritability/relation"
 
 # Define iteration variables.
-readarray -t genes < "$path_dock/split/genes.txt"
+readarray -t genes < "$path_split/genes.txt"
 #count_genes=${#genes[@]}
 #indices=$((count_genes-1))
 #echo $indices
@@ -38,26 +41,18 @@ echo "gene: " $gene
 hostname
 date
 
-$path_project/anaconda3/bin/python $path_program/interface.py main --dock $path_dock --permutation --remote --gene $gene
+# read a file with gene's chromosome?
+
+
+# Organize gene-specific paths.
+path_relation_cis="$path_relation/cis/"
+
+$path_python $path_program/interface.py main --dock $path_dock --permutation --remote --gene $gene
 
 date
 
 
-# Organize paths.
-export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
-path_user_cellar="/cellar/users/tcwaller"
-path_user_nrnb="/nrnb/users/tcwaller"
 
-path_gcta="$path_user_cellar/gcta_1.92.3beta3/gcta64"
-path_plink="$path_user_cellar/plink2"
-path_dock="$path_user_nrnb/dock"
-
-path_persons="$path_user_cellar/Data/heritability/families_persons.tsv"
-path_genotype_vcf="$path_user_nrnb/gtex_genotype/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.vcf.gz"
-path_genotype_ped="$path_dock/gtex-8_genotype"
-path_relation="$path_dock/relation/autosome"
-
-path_heritability="$path_user_cellar/Data/heritability"
 path_distribution="data_gene_families_persons_signals.tsv"
 
 path_gene_distribution="$path_heritability/$gene/$path_distribution"
