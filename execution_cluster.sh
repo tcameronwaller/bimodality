200931b46b846c90e45f876702305811e9753fb2
T. Cameron Waller <tcameronwaller@gmail.com>
2019-10-02 12:55:16 -0700

    modified execution script to select new genes

--- execution_cluster.sh
+++ execution_cluster.sh
@@ -4,40 +4,44 @@
 #SBATCH --output=/cellar/users/tcwaller/Data/dock/out.txt
 #SBATCH --error=/cellar/users/tcwaller/Data/dock/error.txt
 #SBATCH --mem=10G
-#SBATCH --array=14100-15445%100
+#SBATCH --array=0-15445%300
 #SBATCH --time=5-00:00:00 # days-hours:minutes:seconds
 
 # Organize paths.
 export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
-path_project="/cellar/users/tcwaller"
-subpath_repository="repository/bimodality-master/bimodality"
-path_repository="$path_project/$subpath_repository"
-subpath_program="repository/bimodality-master/bimodality"
-path_program="$path_project/$subpath_program"
-subpath_dock="Data/dock"
-path_dock="$path_project/$subpath_dock"
+path_user_cellar="/cellar/users/tcwaller"
+path_python="$path_user_cellar/anaconda3/bin/python"
+path_repository="$path_user_cellar/repository"
+path_program="$path_repository/bimodality-master/bimodality"
+path_dock="$path_user_cellar/Data/dock"
+path_split="$path_dock/split"
+path_distribution="$path_dock/distribution"
+path_permutation="$path_dock/permutation"
 
 # Define iteration variables.
-readarray -t genes < "$path_dock/split/genes.txt"
+readarray -t genes < "$path_split/genes.txt"
 #count_genes=${#genes[@]}
 #indices=$((count_genes-1))
 #echo $indices
 #echo $count_genes
 #12824
-
 #echo ${genes[@]}
 #echo $genes[0]
 
+# Iterate on genes.
 gene=${genes[$SLURM_ARRAY_TASK_ID]}
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "gene: " $gene
 
-# Execute program.
-# Specify complete path to python installation.
+path_permutation_gene="$path_permutation/$gene"
 
 hostname
 date
 
-$path_project/anaconda3/bin/python $path_program/interface.py main --dock $path_dock --permutation --remote --gene $gene
-
-date
+# Determine whether a directory already exists for the current gene.
+if [ ! -d $path_permutation_gene ]
+then
+    # Directory does not already exist for the gene.
+    # Execute permutation procedure.
+    $path_python $path_program/interface.py main --dock $path_dock --permutation --remote --gene $gene
+fi
