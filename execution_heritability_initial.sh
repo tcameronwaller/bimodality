a7177426c34c7d399d096b36bb4e4681c1be3ca1
T. Cameron Waller <tcameronwaller@gmail.com>
2019-10-09 14:18:43 -0700

    prepared script to calculate GRM

--- execution_heritability_initial.sh
+++ execution_heritability_initial.sh
@@ -6,21 +6,17 @@
 export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
 path_user_cellar="/cellar/users/tcwaller"
 path_user_nrnb="/nrnb/users/tcwaller"
+path_dock="$path_user_cellar/Data/dock"
+path_dock_gtex_8="$path_dock/gtex-8"
+path_relation="$path_dock_gtex_8/relation"
+
+path_gtex_8="$path_user_nrnb/gtex-8"
+path_genotype_vcf="$path_gtex_8/gtex-8_genotype.vcf.gz"
+path_genotype_ped="$path_gtex_8/gtex-8_genotype"
+
 path_gcta="$path_user_cellar/gcta_1.92.3beta3/gcta64"
 path_plink="$path_user_cellar/plink2"
 
-path_genotype="$path_user_nrnb/gtex_genotype"
-path_genotype_vcf="$path_genotype/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.vcf.gz"
-path_genotype_ped="$path_genotype/gtex-8_genotype"
-
-path_dock="$path_user_cellar/Data/dock"
-path_selection="$path_dock/selection"
-path_persons="$path_selection/families.tsv"
-path_heritability="$path_dock/heritability"
-path_relation="$path_heritability/relation"
-
-rm -r $path_heritability
-mkdir $path_heritability
 rm -r $path_relation
 mkdir $path_relation
 
@@ -49,4 +45,4 @@ $path_plink --vcf $path_genotype_vcf --make-bed --out $path_genotype_ped --threa
 
 ##########
 # Generate GRM for all autosomal chromosomes.
-$path_gcta --bfile $path_genotype_ped --keep $path_persons --autosome --maf 0.01 --make-grm --out $path_relation/autosome --threads 10
+$path_gcta --bfile $path_genotype_ped --autosome --maf 0.01 --make-grm --out $path_relation/autosome --threads 10
