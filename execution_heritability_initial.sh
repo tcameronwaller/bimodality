acac0a6ab8c69e96e3c5e7adc69d712e963c054e
T. Cameron Waller <tcameronwaller@gmail.com>
2019-10-09 16:17:02 -0700

    included grm for rare variants and principal components

--- execution_heritability_initial.sh
+++ execution_heritability_initial.sh
@@ -12,7 +12,7 @@ path_relation="$path_dock_gtex_8/relation"
 
 path_gtex_8="$path_user_nrnb/gtex-8"
 path_genotype_vcf="$path_gtex_8/gtex-8_genotype.vcf.gz"
-path_genotype_ped="$path_gtex_8/gtex-8_genotype"
+path_genotype_bed="$path_gtex_8/gtex-8_genotype"
 
 path_gcta="$path_user_cellar/gcta_1.92.3beta3/gcta64"
 path_plink="$path_user_cellar/plink2"
@@ -41,8 +41,16 @@ set -x
 # GCTA requires PLINK 1 format files, .bed, .bim, and .fam.
 #$path_plink --vcf $path_genotype_vcf --no-fid --make-bed --out $path_genotype_ped
 # I think that the "no-fid" flag does not change anything when importing a VCF.
-$path_plink --vcf $path_genotype_vcf --make-bed --out $path_genotype_ped --threads 10
+#$path_plink_2 --vcf $path_genotype_vcf --make-bed --out $path_genotype_bed --threads 10
+
 
 ##########
 # Generate GRM for all autosomal chromosomes.
-$path_gcta --bfile $path_genotype_ped --autosome --maf 0.01 --make-grm --out $path_relation/autosome --threads 10
+$path_gcta --bfile $path_genotype_bed --autosome --maf 0.01 --make-grm --out $path_relation/autosome_common --threads 10
+$path_gcta --bfile $path_genotype_bed --autosome --make-grm --out $path_relation/autosome_rare-common --threads 10
+
+##########
+# Calculate principal components.
+# Preserve rare variants in this analysis (do not filter by minor allele
+# frequency).
+$path_gcta --grm $path_relation/autosome_rare-common --pca 10 --out $path_relation/autosome_rare-common
