c2cb378ad0f57dfcaf08f36e87fdb00f54bb4deb
T. Cameron Waller <tcameronwaller@gmail.com>
2019-10-10 12:25:00 -0700

    restored scripts

--- execution_heritability_initial.sh
+++ execution_heritability_initial.sh
@@ -18,20 +18,8 @@ path_plink_1="$path_user_cellar/plink"
 path_plink_2="$path_user_cellar/plink2"
 path_gcta="$path_user_cellar/gcta_1.92.4beta/gcta64"
 
-#rm -r $path_relation
-#mkdir $path_relation
-
-# Temporary test directories...
-path_relation_gcta="$path_relation/gcta"
-path_relation_plink_1="$path_relation/plink_1"
-path_relation_plink_2="$path_relation/plink_2"
-
-rm -r $path_relation_gcta
-rm -r $path_relation_plink_1
-rm -r $path_relation_plink_2
-mkdir $path_relation_gcta
-mkdir $path_relation_plink_1
-mkdir $path_relation_plink_2
+rm -r $path_relation
+mkdir $path_relation
 
 # Suppress echo each command to console.
 set +x
@@ -54,20 +42,21 @@ set -x
 # GCTA requires PLINK 1 format files, .bed, .bim, and .fam.
 #$path_plink --vcf $path_genotype_vcf --no-fid --make-bed --out $path_genotype_ped
 # I think that the "no-fid" flag does not change anything when importing a VCF.
-#$path_plink_2 --vcf $path_genotype_vcf --make-bed --out $path_genotype_bed --threads 10
+$path_plink_2 --vcf $path_genotype_vcf --make-bed --out $path_genotype_bed --threads 10
 
 
 ##########
 # Generate GRM for all autosomal chromosomes.
-#$path_gcta --bfile $path_genotype_bed --autosome --maf 0.01 --make-grm --out $path_relation/autosome_common --threads 10
+$path_gcta --bfile $path_genotype_bed --autosome --maf 0.01 --make-grm --out $path_relation/autosome_common --threads 10
 #$path_gcta --bfile $path_genotype_bed --autosome --make-grm --out $path_relation/autosome_rare-common --threads 10
 
 ##########
 # Calculate principal components.
-# Preserve rare variants in this analysis (do not filter by minor allele
-# frequency).
-$path_gcta --grm $path_relation/autosome_rare-common --pca 10 --out $path_relation_gcta/autosome_rare-common
-$path_gcta --grm $path_relation/autosome_common --pca 10 --out $path_relation_gcta/autosome_common
-
+# Plink2 does not support principal components yet.
+# Plink1 gives memory error when trying to calculate principal components.
 #$path_plink_1 --bfile $path_genotype_bed --autosome --maf 0.01 --pca 10 "header" "tabs" --out $path_relation_plink_1/autosome_common
-#$path_plink_2 --bfile $path_genotype_bed --autosome --maf 0.01 --pca 10 "header" "tabs" --out $path_relation_plink_2/autosome_common
+# Use the Genetic Relationship Matrix (GRM) that does not include rare variants
+# (minor allele frequence < 0.01).
+# Principal components on rare variants produces Eigenvalues near zero and
+# missing values across Eigenvectors.
+$path_gcta --grm $path_relation/autosome_common --pca 10 --out $path_relation/autosome_common
