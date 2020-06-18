0463e5b60874489a8f58bee4af4734a425052a91
T. Cameron Waller <tcameronwaller@gmail.com>
2020-06-18 16:37:51 -0700

    decompressed source files so changed paths

--- access_private.sh
+++ access_private.sh
@@ -11,8 +11,8 @@ export PATH=/cellar/users/tcwaller/anaconda3/bin:$PATH
 path_gtex="/nrnb/data/controlled/2020_dbGaP_GTEx/75875/PhenoGenotypeFiles/RootStudyConsentSet_phs000424.GTEx.v8.p2.c1.GRU"
 path_origin_ancestry="/nrnb/users/mpagadal/gtex/gtex-ancestry/sklearn-gtex-ancestry"
 path_origin_attributes="$path_gtex/PhenotypeFiles"
-path_origin_attribute_sample="$path_origin_attributes/phs000424.v8.pht002743.v8.p2.c1.GTEx_Sample_Attributes.GRU.txt.gz"
-path_origin_attribute_person="$path_origin_attributes/phs000424.v8.pht002742.v8.p2.c1.GTEx_Subject_Phenotypes.GRU.txt.gz"
+path_origin_attribute_sample="$path_origin_attributes/phs000424.v8.pht002743.v8.p2.c1.GTEx_Sample_Attributes.GRU.txt"
+path_origin_attribute_person="$path_origin_attributes/phs000424.v8.pht002742.v8.p2.c1.GTEx_Subject_Phenotypes.GRU.txt"
 path_origin_explanation_sample="$path_origin_attributes/phs000424.v8.pht002743.v8.GTEx_Sample_Attributes.data_dict.xml"
 path_origin_explanation_person="$path_origin_attributes/phs000424.v8.pht002742.v8.GTEx_Subject_Phenotypes.data_dict.xml"
 
